6ae91aa064cbd96dc896ce05563ea4f00e8a62c7
matteobarone <barmat90@gmail.com>
2018-09-30 23:26:13 +0200

    test(snake): reducer

--- src/app/game/store/reducers/snake.reducer.spec.ts
+++ src/app/game/store/reducers/snake.reducer.spec.ts
@@ -1,5 +1,16 @@
+import {Snake, snakeReducer} from "./snake.reducer";
+import {AddBlock} from "../actions/snake.actions";
+
 describe('snakeReducer', () => {
   it('should add a block', () => {
+    const initialState: Snake = {
+      blocks: 3,
+      direction: 'RIGHT',
+      headPosition: [3,3],
+    };
+    const newState: Snake = snakeReducer(initialState, new AddBlock());
+    expect(newState.blocks).toBe(4);
+
     // console.log('add block');
   })
 });
